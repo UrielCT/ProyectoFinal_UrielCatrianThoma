10953d08a990cb4fa9ecf717a1495f3f8570c430
Uriel thoma <108682061+UrielCT@users.noreply.github.com>
2022-12-06 12:01:45 -0300

    creo el header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoHeaderComponent } from './components/logo-header/logo-header.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoHeaderComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
